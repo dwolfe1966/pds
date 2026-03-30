fed7256d716ef4711ece3e0575332869f936fe8d
David Wolfe <dwolfe66@hotmail.com>
2026-03-30 15:20:24 -0700

    nav change

--- src/components/AdminNav.js
+++ src/components/AdminNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import logoSrc from '../assets/idlookup-green-logo-no-words.png';
 
 /**
  * Navigation bar for administrator pages.
